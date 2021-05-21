fa83e11965f56ab5cd78d7b384c888b1b178a55a
rasulkurbanov <rasuljohn139@gmail.com>
2021-05-22 02:13:31 +0500

    Add URL configuration

--- movies/urls.py
+++ movies/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+
+app_name = 'movies'
 urlpatterns = [
-  path('', views.index, name='index')
-  ]
-
-
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
+]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
+from django import http
 from movies.models import Genres, Movie
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
 
@@ -9,4 +10,9 @@ def index(request):
     # output = ', '.join([m.title for m in movies])
     # return HttpResponse("Hello World, You are at my first django project")
     # return HttpResponse(output)
-    return render(request, 'movies/base.html', { 'movies': movies })
+    return render(request, 'movies/base.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
